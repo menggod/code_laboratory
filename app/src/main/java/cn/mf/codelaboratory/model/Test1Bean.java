5a20417c4a458dc7e75b0dcd831ffa1d7e85442e
menggod <menggod@126.com>
2018-01-02 14:59:15 +0800

    backup

---
+++ app/src/main/java/cn/mf/codelaboratory/model/Test1Bean.java
@@ -0,0 +1,13 @@
+package cn.mf.codelaboratory.model;
+
+import java.io.Serializable;
+
+/**
+ * 项目名称：CodeLaboratory
+ *
+ * @author menggod
+ * @date 2017/12/20
+ */
+public class Test1Bean implements Serializable {
+
+}
