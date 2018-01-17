027932b00e6cbabc557a5f980dfd7dcd43e58b61
menggod <menggod@126.com>
2018-01-17 17:42:38 +0800

    backup

--- app/src/main/java/cn/mf/codelaboratory/view/RatioCircleColorView.java
+++ app/src/main/java/cn/mf/codelaboratory/view/RatioCircleColorView.java
@@ -27,6 +27,12 @@ public class RatioCircleColorView extends ImageView {
     private int mColor;
     private int mHight;
 
+    //外圈白线和内圈黑线边框之间的间隔。
+    private int gap = 25;
+
+    //平衡在计算半径时候因为除以2导致半径计算的误差
+    private int balance_factor = 2;
+
     public RatioCircleColorView(Context context) {
         super(context);
         setColor();
@@ -102,8 +108,40 @@ public class RatioCircleColorView extends ImageView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //drawOutWhiteCircle(canvas,2);
+        drawInnerBlackCircle(canvas,2);
+    }
+
+    private void drawOutWhiteCircle(Canvas canvas, int strokeWidth) {
+        Paint paint = new Paint();
+        int parseColor = Color.parseColor("#234567");
+        paint.setColor(parseColor);
+        paint.setStrokeWidth(strokeWidth);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+        int w = this.getPaddingLeft() + this.getPaddingRight();
+        int x = getWidth() - w;
+        int r = x / 2;
+
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, r + strokeWidth + 1, paint);
+    }
 
 
+    //画最内层的黑色边框
+    private void drawInnerBlackCircle(Canvas canvas, int strokeWidth) {
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(strokeWidth);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+        int w = this.getPaddingLeft() + this.getPaddingRight();
+        float x = getWidth() - w;
+        float r = x / 2;
+
+        //半径减去平衡因子是因为在取整时候四舍五入
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, (r - balance_factor) + strokeWidth, paint);
     }
 
 
